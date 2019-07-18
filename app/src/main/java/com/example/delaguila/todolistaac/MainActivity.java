39d2a9b0ec04672b7a6ede551225a35fae5b60ca
Jhordy Alexi <jhordydelaguila@hotmail.com>
2019-07-18 12:19:03 -0500

    Add(View) View for AddTaskACtivity

--- app/src/main/java/com/example/delaguila/todolistaac/MainActivity.java
+++ app/src/main/java/com/example/delaguila/todolistaac/MainActivity.java
@@ -1,16 +1,31 @@
 package com.example.delaguila.todolistaac;
 
+import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Constant for logging
+    private static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
+        FloatingActionButton faButton = findViewById(R.id.fab);
+        faButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent addTaskIntent = new Intent(MainActivity.this, AddTaskActivity.class);
+                startActivity(addTaskIntent);
+            }
+        });
+
+    }
 
 
 }
